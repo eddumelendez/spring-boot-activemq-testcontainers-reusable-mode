a87a369d873626ff54b4821c30f6d2722b1fa988
Eddú Meléndez <eddu.melendez@gmail.com>
2023-07-23 00:10:37 -0600

    Initial commit

---
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+
+public class ProducerApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProducerApplication::main)
+				.with(ContainerConfiguration.class)
+				.run(args);
+	}
+
+	@TestConfiguration
+	static class ContainerConfiguration {
+
+		@Bean
+		@ServiceConnection(name = "symptoma/activemq")
+		@RestartScope
+		GenericContainer<?> activeMQContainer() {
+			return new GenericContainer<>("symptoma/activemq:5.18.0")
+					.withExposedPorts(61616)
+					.withReuse(true);
+		}
+
+	}
+
+}
